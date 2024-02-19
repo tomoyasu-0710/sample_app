50135c8abd23fac7faaeb3b43f79ceb3c1175358
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-02-19 08:54:47 +0000

    [start]3-10

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
     #Viewへ渡すため渡すためのインスタンス変数にからのModelオブジェクトを生成する
     @list = List.new
   end
-  
+
   #以下追加
   def create
     #１。＆２データを受け取り新規登録するためのインスタンス作成
@@ -28,19 +28,19 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id]) #データ（レコード）を一件取得
     list.destroy   #データ（レコード）を削除
     redirect_to '/lists'  #投稿一覧画面にリダイレクト
   end
-  
+
   private
   #ストロングパラメータ
   def list_params
